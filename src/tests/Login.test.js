1ebd5e55569b0884bc954524e00e5141ff8c43e8
Lucasteisouza <lucasteisouza@gmail.com>
2023-01-25 20:47:02 -0300

    feat: pagina de login, validação de dados de login e encaminhamento para pagina de receitas

---
+++ src/tests/Login.test.js
@@ -0,0 +1,30 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../helpers/renderWithRouter';
+import App from '../App';
+
+describe('testa a página de login e suas funcionalidades', () => {
+  test('testa a presença dos campos e do botão', () => {
+    renderWithRouter(<App />);
+    const passwordInput = screen.getByTestId('password-input');
+    const emailInput = screen.getByTestId('email-input');
+    const btn = screen.getByRole('button', { name: /entrar/i });
+    expect(passwordInput).toBeInTheDocument();
+    expect(emailInput).toBeInTheDocument();
+    expect(btn).toBeInTheDocument();
+  });
+
+  test('testa se o botao é habilitado ao preencher os campos corretamente e redireciona para /meals', () => {
+    const { history } = renderWithRouter(<App />);
+    const passwordInput = screen.getByTestId('password-input');
+    const emailInput = screen.getByTestId('email-input');
+    const btn = screen.getByRole('button', { name: /entrar/i });
+    expect(btn).toHaveProperty('disabled');
+    userEvent.type(emailInput, 'email@email.com');
+    userEvent.type(passwordInput, '1234567');
+    expect(btn).toHaveProperty('disabled', false);
+    userEvent.click(btn);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/meals');
+  });
+});
