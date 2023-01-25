8865223602000bac9a363b13b9b663d456045ad4
Daniel Ramos Regis <dnlrgs@Daniels-MacBook-Pro.local>
2023-01-25 17:58:30 -0300

    feat: rotas, provider, contextapi criadas

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './pages/Login';
+import RecipeDetails from './pages/RecipeDetails';
+import RecipeInProgress from './pages/RecipeInProgress';
+import Profile from './pages/Profile';
+import Recipes from './pages/Recipes';
+import DoneRecipes from './pages/DoneRecipes';
+import FavoriteRecipes from './pages/FavoriteRecipes';
+import Provider from './context/provider';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <Provider>
+      <Switch>
+
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/meals" component={ Recipes } />
+        <Route exact path="/drinks" component={ Recipes } />
+        <Route exact path="/meals/:id-da-receita" component={ RecipeDetails } />
+        <Route exact path="/drinks/:id-da-receita" component={ RecipeDetails } />
+        <Route
+          exact
+          path="/meals/:id-da-receita/in-progress"
+          component={ RecipeInProgress }
+        />
+        <Route
+          exact
+          path="/drinks/:id-da-receita/in-progress"
+          component={ RecipeInProgress }
+        />
+        <Route exact path="/profile" component={ Profile } />
+        <Route exact path="/done-recipes" component={ DoneRecipes } />
+        <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
+
+      </Switch>
+    </Provider>
+
   );
 }
 
