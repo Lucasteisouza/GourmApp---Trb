e4910576dbab8b31098727d0f0292395f4fa60b7
Daniel Ramos Regis <dnlrgs@Daniels-MacBook-Pro.local>
2023-01-26 01:14:47 -0300

    feat: tela login
    busca email no localstorage e exibe

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Profile() {
+  // const userEmail = 'danieltetetetete@gmail.com';
+  // const saveEmailLocalStorage = (e) => {
+  //   localStorage.setItem('email', JSON.stringify(e));
+  // };
+
+  const [email, setEmail] = useState('');
+
+  useEffect(() => {
+    // saveEmailLocalStorage(userEmail);
+    setEmail(() => {
+      const saved = localStorage.getItem('email');
+      const initialValue = JSON.parse(saved);
+      console.log(initialValue);
+      return initialValue || '';
+    });
+  }, []);
+
   return (
-    <div>Profile</div>
+    <div>
+      Profile
+      <p data-testid="profile-email">{ email }</p>
+      <div>
+        <button data-testid="profile-done-btn">Done Recipes</button>
+        <button data-testid="profile-favorite-btn">Favorite Recipes</button>
+        <button data-testid="profile-logout-btn">Logout</button>
+      </div>
+    </div>
   );
 }
+
+// {
+//   email: email-da-pessoa
+// }
 
 export default Profile;
