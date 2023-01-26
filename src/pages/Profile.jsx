3f18d412cccd20dda970d1431bb81dde6eaf29c3
Daniel Ramos Regis <dnlrgs@Daniels-MacBook-Pro.local>
2023-01-26 16:31:02 -0300

    feat: Implementacao Profile
    Acesso LocalSotorage, botoes e redirecionamento e limpeza do storage em caso de logot

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,30 +1,53 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 function Profile() {
-  // const userEmail = 'danieltetetetete@gmail.com';
-  // const saveEmailLocalStorage = (e) => {
-  //   localStorage.setItem('email', JSON.stringify(e));
-  // };
+  const user = JSON.parse(localStorage.getItem('user')) || '';
+  const history = useHistory();
 
-  const [email, setEmail] = useState(() => { setEmail(); });
+  const handleClickDoneRecipes = () => {
+    history.push('/done-recipes');
+  };
 
-      const user = JSON.parse(localStorage.getItem('user')) || ''
+  const handleClickFavoriteRecipes = () => {
+    history.push('/favorite-recipes');
+  };
+
+  const handleClickLogin = () => {
+    history.push('/');
+    localStorage.clear('user');
+  };
 
   return (
+
     <div>
       Profile
-      <p data-testid="profile-email">{ email }</p>
+      <p data-testid="profile-email">{ user.email }</p>
       <div>
-        <button data-testid="profile-done-btn">Done Recipes</button>
-        <button data-testid="profile-favorite-btn">Favorite Recipes</button>
-        <button data-testid="profile-logout-btn">Logout</button>
+        <button
+          data-testid="profile-done-btn"
+          onClick={ handleClickDoneRecipes }
+        >
+          Done Recipes
+
+        </button>
+        <button
+          data-testid="profile-favorite-btn"
+          onClick={ handleClickFavoriteRecipes }
+        >
+          Favorite Recipes
+
+        </button>
+        <button
+          data-testid="profile-logout-btn"
+          onClick={ handleClickLogin }
+        >
+          Logout
+
+        </button>
       </div>
     </div>
   );
 }
-
-// {
-//   email: email-da-pessoa
-// }
 
 export default Profile;
