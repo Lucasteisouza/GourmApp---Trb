6af5c1fb3add6ca74b4624662d60b1d93d9cd4f7
Vitor Wagner <vitormwagner@gmail.com>
2023-01-25 21:31:08 -0300

    Adiciona testes para componente

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,10 +8,10 @@ function Footer() {
   return (
     <div data-testid="footer" className="footer">
       Footer
-      <Link data-testid="drinks-bottom-btn" to="/drinks">
+      <Link data-testid="drinks-bottom-btn" src={ drinkIcon } to="/drinks">
         <img src={ drinkIcon } alt="Drinks" width="40" />
       </Link>
-      <Link data-testid="meals-bottom-btn" to="/meals">
+      <Link data-testid="meals-bottom-btn" src={ mealIcon } to="/meals">
         <img src={ mealIcon } alt="Meals" width="40" />
       </Link>
     </div>
