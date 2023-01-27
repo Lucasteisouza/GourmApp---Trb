9f8b2e4428f7002b4cf1dc55b4c845de94715e2c
Ikarosv <ikarovieiraa@gmail.com>
2023-01-26 22:13:27 -0300

    refactor: muda renderWithRouter de pasta e remove header.test

--- src/tests/Header.test.js
+++
@@ -1,7 +0,0 @@
-// import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
-// import renderWithRouter from '../helpers/renderWithRouter';
-// import App from '../App';
-
-// describe('testa o componente Header', () => {
-// });
