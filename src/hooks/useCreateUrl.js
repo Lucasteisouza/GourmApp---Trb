aa09bf3f6e6468748f9d5b4a6b716dba4959ed11
Ikarosv <ikarovieiraa@gmail.com>
2023-01-27 20:17:39 -0300

    feat: hook de criar uma url

---
+++ src/hooks/useCreateUrl.js
@@ -0,0 +1,35 @@
+import { useMemo, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+/**
+ * @returns {Array} [url, setEndPoint, setUrl]
+ * @param {object} url {baseUrl, endPoint}
+ * @param {string} url.baseUrl
+ * @param {string} url.endPoint
+ * @param {function} setEndPoint
+ * @param {function} setUrl
+ * @param {URL} fullUrl
+ * @param {function} setFullUrl
+ * @description Hook para criar uma URL para fazer o fetch. A url muda de acordo com o path da página.
+ * @example const [url, setEndPoint, setUrl] = useCreateUrl();
+*/
+export default function useCreateUrl(endPoint) {
+  const history = useHistory();
+  const pathname = history.location.pathname.split('/')[1];
+  const [url, setUrl] = useState({
+    baseUrl: pathname === 'meals'
+      ? 'https://www.themealdb.com/api/json/v1/1/' : 'https://www.thecocktaildb.com/api/json/v1/1/',
+    endPoint,
+  });
+  const [fullUrl, setFullUrl] = useState(`${url.baseUrl}${url.endPoint}`);
+
+  const setEndPoint = (end) => {
+    setUrl({ ...url, endPoint: end });
+    setFullUrl(`${url.baseUrl}${end}`);
+  };
+
+  return useMemo(
+    () => ([url, setEndPoint, fullUrl, setUrl, setFullUrl]),
+    [url, fullUrl, endPoint],
+  );
+}
--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -2,6 +2,21 @@ import React from 'react';
 import Header from '../components/Header';
 
 function RecipeDetails() {
+  const { params } = useRouteMatch();
+  const [,, fullUrl] = useCreateUrl(`lookup.php?i=${params.id}`);
+  const { data, loading, fetchData } = useFetch();
+
+  useState(() => {
+    const makeFetch = async () => {
+      await fetchData(fullUrl);
+    };
+    makeFetch();
+  }, []);
+
+  if (loading || !data) return <Loading />;
+
+  const [details] = data.meals || data.drinks;
+
   return (
     <div>
       <Header />
