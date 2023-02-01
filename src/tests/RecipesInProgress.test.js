a5568004fa360db1b21c5f211c152cc89129fee0
Lucasteisouza <lucasteisouza@gmail.com>
2023-02-01 14:09:49 -0300

    feat: adiciona testes até atingir ~70% de cobertura no componente RecipesInProgress

---
+++ src/tests/RecipesInProgress.test.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import userEvent from '@testing-library/user-event';
+import oneMeal from './mocks/oneMeal';
+import renderWithRouter from './helpers/renderWithRouter';
+import App from '../App';
+
+const storedDoneRecicepsMock = { alcoholicOrNot: '',
+  category: 'Vegetarian',
+  doneDate: '',
+  id: '52771',
+  image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+  name: 'Spicy Arrabiata Penne',
+  nationality: 'Italian',
+  tags: 'Pasta,Curry',
+  type: 'meals',
+};
+
+describe('Testa a tela in-progress', () => {
+  beforeEach(() => {
+    jest.spyOn(global, 'fetch').mockResolvedValue({
+      json: jest.fn().mockResolvedValue(oneMeal),
+    });
+  });
+
+  it('testa se a receita correta é renderizada na página /52771/in-progress', async () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => history.push('/meals/52771/in-progress'));
+    const arrabiata = await screen.findByRole('heading', { name: /spicy arrabiata penne/i });
+    expect(arrabiata).toBeInTheDocument();
+  });
+
+  it('testa se o botão de finalizar receita é desbloqueado ao selecionar todos os ingredientes e salva a receina no localStorage', async () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => history.push('/meals/52771/in-progress'));
+    const finishBtn = await screen.findByRole('button', { name: /finalizar receita/i });
+    expect(finishBtn).toBeInTheDocument();
+    expect(finishBtn).toHaveProperty('disabled');
+    const checkboxes = await screen.findAllByRole('checkbox');
+    expect(checkboxes.length).toBe(8);
+    userEvent.click(checkboxes[0]);
+    userEvent.click(checkboxes[1]);
+    userEvent.click(checkboxes[2]);
+    userEvent.click(checkboxes[3]);
+    userEvent.click(checkboxes[4]);
+    userEvent.click(checkboxes[5]);
+    userEvent.click(checkboxes[6]);
+    userEvent.click(checkboxes[7]);
+    expect(finishBtn).toHaveProperty('disabled', false);
+    userEvent.click(checkboxes[0]);
+    expect(finishBtn).toHaveProperty('disabled');
+    userEvent.click(checkboxes[0]);
+    userEvent.click(finishBtn);
+    const doneRecipes = await screen.findByRole('heading', { name: /done recipes/i });
+    expect(doneRecipes).toBeInTheDocument();
+    expect(history.location.pathname).toBe('/done-recipes');
+  });
+
+  it('testa se o botão de finalizar receita é desbloqueado ao selecionar todos os ingredientes e salva a receina no localStorage', async () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => history.push('/meals/52771/in-progress'));
+    const finishBtn = await screen.findByRole('button', { name: /finalizar receita/i });
+    expect(finishBtn).toBeInTheDocument();
+    expect(finishBtn).toHaveProperty('disabled');
+    const checkboxes = await screen.findAllByRole('checkbox');
+    expect(checkboxes.length).toBe(8);
+    userEvent.click(checkboxes[0]);
+    userEvent.click(checkboxes[1]);
+    userEvent.click(checkboxes[2]);
+    userEvent.click(checkboxes[3]);
+    userEvent.click(checkboxes[4]);
+    userEvent.click(checkboxes[5]);
+    userEvent.click(checkboxes[6]);
+    userEvent.click(checkboxes[7]);
+    expect(finishBtn).toHaveProperty('disabled', false);
+    userEvent.click(checkboxes[0]);
+    expect(finishBtn).toHaveProperty('disabled');
+    userEvent.click(checkboxes[0]);
+    userEvent.click(finishBtn);
+    const doneRecipes = await screen.findByRole('heading', { name: /done recipes/i });
+    expect(doneRecipes).toBeInTheDocument();
+    expect(history.location.pathname).toBe('/done-recipes');
+  });
+
+  // test('testa se a aplicação detecta a existencia da receita no localStorage', async () => {
+  //   const { history } = renderWithRouter(<App />);
+  //   localStorage.setItem('checkerList', { meals: {}, drinks: {} });
+  //   act(() => history.push('/meals/52771/in-progress'));
+    // const currentLocal = JSON.parse(localStorage.getItem('checkerList'));
+    // console.log(currentLocal);
+  // });
+});
